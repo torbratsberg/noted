b1be306147d142467e6b5ab6f9843ad6bd074c85
Tor Bratsberg <tor@bratsberg.net>
2022-03-17 13:57:16 +0100

    Improved layout and custom glamour renderer

--- go.mod
+++ go.mod
@@ -23,7 +23,8 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/yuin/goldmark v1.4.4 // indirect
 	github.com/yuin/goldmark-emoji v1.0.1 // indirect
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 )
--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const notesFolder = "/Users/torbratsberg/main/notes/"
@@ -22,6 +24,7 @@ type model struct {
 	listView     viewport.Model
 	pointer      int
 	ready        bool
+	glammy       glamour.TermRenderer
 	cachedFiles  map[string]string
 }
 
@@ -37,10 +40,6 @@ func (m model) RenderList() string {
 	listStr := ""
 
 	for i, note := range m.notes {
-		if note.IsDir() {
-			continue
-		}
-
 		if i == m.pointer {
 			listStr += fmt.Sprintf("> %s\n", note.Name())
 		} else {
@@ -52,18 +51,35 @@ func (m model) RenderList() string {
 }
 
 func (m model) RenderFile() string {
+	// Check if the file is cached
 	if m.cachedFiles[m.notes[m.pointer].Name()] != "" {
 		return m.cachedFiles[m.notes[m.pointer].Name()]
 	}
 
+	// Read and render the file
 	content, err := ioutil.ReadFile(notesFolder + m.notes[m.pointer].Name())
 	check(err)
-	out, err := glamour.Render(string(content), "dark")
+	out, err := m.glammy.Render(string(content))
 	check(err)
 
+	// Cache the file
 	m.cachedFiles[m.notes[m.pointer].Name()] = out
 
 	return out
+}
+
+func (m model) NewFile() {
+	// Open the file in $EDITOR
+	cmd := exec.Command(
+		os.Getenv("EDITOR"),
+		notesFolder+"my_new_note.md",
+	)
+	m.listView.SetContent(cmd.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Run()
+	check(err)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -90,10 +106,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.readerView.HalfViewDown()
 		} else if k == "u" {
 			m.readerView.HalfViewUp()
+		} else if k == "n" {
+			m.NewFile()
+
+			// } else if k == "d" {
+			// 	err := os.Remove(notesFolder + m.notes[m.pointer].Name())
+			// 	check(err)
 		}
 
 		if k == "k" || k == "j" {
-			m.listView.SetContent("\n===\n\n" + m.RenderList())
+			m.listView.SetContent(fmt.Sprintf(
+				"\n\n%s\n\n%s",
+				strings.Repeat("=", m.readerView.Width),
+				m.RenderList()))
 			m.readerView.SetContent(m.RenderFile())
 		}
 
@@ -112,11 +137,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return m, tea.Quit
 			}
 
-			// go func() {
-			// 	err := cmd.Run()
-			// 	check(err)
-			// }()
-
 			return m, tea.Quit
 		}
 
@@ -124,18 +144,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case tea.WindowSizeMsg:
 		if !m.ready {
-			m.readerView = viewport.New(msg.Width/2, msg.Height/2)
+			m.readerView = viewport.New(msg.Width, msg.Height/4*3)
 			m.readerView.SetContent(m.fileContents)
 
-			m.listView = viewport.New(msg.Width/2, msg.Height/2)
-			m.listView.SetContent("\n===\n\n" + m.RenderList())
+			m.listView = viewport.New(msg.Width, msg.Height/4)
+			m.listView.SetContent(fmt.Sprintf(
+				"\n\n%s\n\n%s",
+				strings.Repeat("=", m.readerView.Width),
+				m.RenderList()))
 
 			m.ready = true
 		} else {
-			m.listView.Width = msg.Width / 2
-			m.listView.Height = msg.Height / 2
-			m.readerView.Width = msg.Width / 2
-			m.readerView.Height = msg.Height / 2
+			m.listView.Width = msg.Width
+			m.listView.Height = msg.Height / 4
+			m.readerView.Width = msg.Width
+			m.readerView.Height = msg.Height / 4 * 3
 		}
 	}
 
@@ -156,6 +179,7 @@ func (m model) View() string {
 }
 
 func main() {
+
 	notes, err := os.ReadDir("/Users/torbratsberg/main/notes/")
 	check(err)
 
@@ -169,22 +193,26 @@ func main() {
 	}
 	notes = notes[:n]
 
+	termWidth, _, err := terminal.GetSize(0)
+	check(err)
+	glammy, err := glamour.NewTermRenderer(
+		glamour.WithAutoStyle(),
+		glamour.WithWordWrap(termWidth-3),
+	)
+
 	// Load some text for our viewport
 	var content string
 	if len(notes) > 0 {
 		contentBytes, err := ioutil.ReadFile(notesFolder + notes[0].Name())
 		check(err)
-		parsed, err := glamour.Render(string(contentBytes), "dark")
+		parsed, err := glammy.Render(string(contentBytes))
 		check(err)
 		content = parsed
 	} else {
-		parsed, err := glamour.Render(
-			fmt.Sprintf(
-				"No notes found in %s.\nPress `n` to create a new note.\n",
-				notesFolder,
-			),
-			"dark",
-		)
+		parsed, err := glammy.Render(fmt.Sprintf(
+			"No notes found in %s.\nPress `n` to create a new note.\n",
+			notesFolder,
+		))
 
 		check(err)
 		content = parsed
@@ -195,6 +223,7 @@ func main() {
 			fileContents: string(content),
 			notes:        notes,
 			cachedFiles:  make(map[string]string, len(notes)),
+			glammy:       *glammy,
 		},
 		tea.WithAltScreen(),
 		tea.WithMouseCellMotion(),
